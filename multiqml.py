568fb6a3e92fb480e712d39f5feba581fe5ed891
agentili <agentili@navionics.com>
2015-06-11 09:17:53 +0200

    Update multiqml.py
    Found and fix the issue in the "isRasterQml". No modification in line 97, the logic is the same.
    Into qml for rasters there is not the "<rasterproperties>" tag.
    I add "<rasterrenderer" tag actually present
    
    I'm using Qgis 2.8.2 Wien

--- multiqml.py
+++ multiqml.py
@@ -74,7 +74,7 @@ class MultiQmlDlg(QDialog, Ui_MultiQmlForm):
       line = qmlFile.readline()
       result = False
       while line != "":
-        if "<rasterproperties>" in line:
+        if "<rasterproperties>" in line or "<rasterrenderer" in line:
           result = True
           break
         line = qmlFile.readline()
