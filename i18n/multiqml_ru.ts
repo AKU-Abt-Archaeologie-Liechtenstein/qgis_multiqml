689d29e51bbf2929ac02043c4108e7408fa43a1d
shachinandan <sergey.taranenko@gmail.com>
2022-11-24 16:49:40 +0700

    Fixed switching of interface languages.

--- i18n/multiqml_ru.ts
+++ i18n/multiqml_ru.ts
@@ -4,60 +4,57 @@
 <context>
     <name>MultiQmlDlg</name>
     <message>
-        <location filename="../multiqml.py" line="63"/>
+        <location filename="multiqml.py" line="63"/>
         <source>MultiQML</source>
         <translation>MultiQML</translation>
     </message>
     <message>
-        <location filename="../multiqml.py" line="64"/>
+        <location filename="multiqml.py" line="64"/>
         <source>Enabling this option will cause overwriting of any existing QML files. Are you sure?</source>
         <translation>Включение приведет к перезаписи существующих файлов QML. Вы уверены?</translation>
     </message>
     <message>
-        <location filename="../multiqml.py" line="85"/>
+        <location filename="multiqml.py" line="85"/>
         <source>Open style</source>
         <translation>Открыть стиль</translation>
     </message>
     <message>
-        <location filename="../multiqml.py" line="85"/>
+        <location filename="multiqml.py" line="85"/>
         <source>QGIS apply style file (*.qml)</source>
         <translation>QGIS применить стиль (*.qml)</translation>
     </message>
     <message>
-        <location filename="../multiqml.py" line="94"/>
+        <location filename="multiqml.py" line="94"/>
         <source>Unable to apply raster qml style &quot;%1&quot; to vector layer &quot;%2&quot;.</source>
         <translation>Невозможно применить растровый qml стиль &quot;%1&quot; к векторному слою &quot;%2&quot;.</translation>
     </message>
     <message>
-        <location filename="../multiqml.py" line="98"/>
+        <location filename="multiqml.py" line="98"/>
         <source>Unable to apply vector qml style &quot;%1&quot; to raster layer &quot;%2&quot;.</source>
         <translation>Невозможно применить векторный qml стиль &quot;%1&quot; к растровому слою &quot;%2&quot;.</translation>
     </message>
     <message>
-        <location filename="../multiqml.py" line="105"/>
-        <source>Unable to apply qml style &quot;%1&quot; to layer &quot;%2&quot;
-%3.</source>
-        <translation>Невозможно применить qml стиль &quot;%1&quot; к слою &quot;%2&quot;
-%3.</translation>
+        <location filename="multiqml.py" line="105"/>
+        <source>Unable to apply qml style &quot;%1&quot; to layer &quot;%2&quot;%3.</source>
+        <translation>Невозможно применить qml стиль &quot;%1&quot; к слою &quot;%2&quot;%3.</translation>
     </message>
     <message>
-        <location filename="../multiqml.py" line="116"/>
+        <location filename="multiqml.py" line="116"/>
         <source>A style was not applied.</source>
         <translation>Стиль не был применен.</translation>
     </message>
     <message>
-        <location filename="../multiqml.py" line="125"/>
-        <source>Unable to restory an initial style for layer &quot;%1&quot;
-%2.</source>
+        <location filename="multiqml.py" line="125"/>
+        <source>Unable to restory an initial style for layer &quot;%1&quot;%2.</source>
         <translation>Невозможно восстановить начальный стиль для слоя &quot;%1&quot;%2.</translation>
     </message>
     <message>
-        <location filename="../multiqml.py" line="228"/>
+        <location filename="multiqml.py" line="228"/>
         <source>Information</source>
         <translation>Информация</translation>
     </message>
     <message>
-        <location filename="../multiqml.py" line="230"/>
+        <location filename="multiqml.py" line="230"/>
         <source>Error</source>
         <translation>Ошибка</translation>
     </message>
@@ -65,47 +62,47 @@
 <context>
     <name>MultiQmlForm</name>
     <message>
-        <location filename="../multiqml.ui" line="20"/>
+        <location filename="multiqml.ui" line="20"/>
         <source>Assign style</source>
         <translation>Установить стиль</translation>
     </message>
     <message>
-        <location filename="../multiqml.ui" line="41"/>
+        <location filename="multiqml.ui" line="41"/>
         <source>Layers:</source>
         <translation>Слои:</translation>
     </message>
     <message>
-        <location filename="../multiqml.ui" line="51"/>
+        <location filename="multiqml.ui" line="51"/>
         <source>Raster</source>
         <translation>Растровые</translation>
     </message>
     <message>
-        <location filename="../multiqml.ui" line="64"/>
+        <location filename="multiqml.ui" line="64"/>
         <source>Vector</source>
         <translation>Векторные</translation>
     </message>
     <message>
-        <location filename="../multiqml.ui" line="92"/>
+        <location filename="multiqml.ui" line="92"/>
         <source>Apply style ...</source>
         <translation>Применить стиль ...</translation>
     </message>
     <message>
-        <location filename="../multiqml.ui" line="99"/>
+        <location filename="multiqml.ui" line="99"/>
         <source>Restore initial style</source>
         <translation>Восстановить начальный стиль</translation>
     </message>
     <message>
-        <location filename="../multiqml.ui" line="106"/>
+        <location filename="multiqml.ui" line="106"/>
         <source>Select all layers</source>
         <translation>Выбрать все слои</translation>
     </message>
     <message>
-        <location filename="../multiqml.ui" line="113"/>
+        <location filename="multiqml.ui" line="113"/>
         <source>Close</source>
         <translation>Закрыть</translation>
     </message>
     <message>
-        <location filename="../multiqml.ui" line="122"/>
+        <location filename="multiqml.ui" line="122"/>
         <source>Save as default</source>
         <translation>Сохранить по умолчанию</translation>
     </message>
@@ -113,7 +110,7 @@
 <context>
     <name>MultiQmlPlugin</name>
     <message>
-        <location filename="../plugin.py" line="71"/>
+        <location filename="plugin.py" line="71"/>
         <source>MultiQml</source>
         <translation>MultiQml</translation>
     </message>
@@ -122,15 +119,15 @@
         <translation type="obsolete">Применить один qml стиль к нескольким растровым или векторным слоям.</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="73"/>
+        <location filename="plugin.py" line="73"/>
         <source>About</source>
         <translation>Информация</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="79"/>
-        <location filename="../plugin.py" line="80"/>
-        <location filename="../plugin.py" line="85"/>
-        <location filename="../plugin.py" line="86"/>
+        <location filename="plugin.py" line="79"/>
+        <location filename="plugin.py" line="80"/>
+        <location filename="plugin.py" line="85"/>
+        <location filename="plugin.py" line="86"/>
         <source>&amp;MultiQml</source>
         <translation>&amp;MultiQml</translation>
     </message>
@@ -143,12 +140,12 @@
         <translation type="obsolete">    Плагин QGIS для применения одного qml стиля к нескольким растровым или векторным слоям.</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="104"/>
+        <location filename="plugin.py" line="104"/>
         <source>&lt;b&gt;Developers:&lt;/b&gt;</source>
         <translation>&lt;b&gt;Разработчики:&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="108"/>
+        <location filename="plugin.py" line="108"/>
         <source>&lt;b&gt;Link:&lt;/b&gt;</source>
         <translation>&lt;b&gt;Ссылка:&lt;/b&gt;</translation>
     </message>
@@ -159,7 +156,7 @@
     к нескольким растровым или векторным слоям.</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="99"/>
+        <location filename="plugin.py" line="99"/>
         <source>About</source>
         <comment>Window title</comment>
         <translation>Информация</translation>
@@ -173,24 +170,24 @@
         <translation type="obsolete">sdfd</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="102"/>
+        <location filename="plugin.py" line="102"/>
         <source>&lt;b&gt;MultiQml (Version %1):&lt;/b&gt;</source>
         <translation>&lt;b&gt;MultiQml (Версия %1):&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="72"/>
+        <location filename="plugin.py" line="72"/>
         <source>Apply single qml style to multiple raster or vector layers</source>
         <translation>Применить один qml стиль к нескольким растровым или векторным слоям</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="103"/>
+        <location filename="plugin.py" line="103"/>
         <source>    This plugin takes single qml style and
 applies it to multiple raster or vector layers</source>
         <translation>Этот плагин применяет один стиль из файла Qml
 к нескольким растровым или векторный слоям</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="116"/>
+        <location filename="plugin.py" line="116"/>
         <source>Close</source>
         <translation>Закрыть</translation>
     </message>
